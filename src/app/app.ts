305a5827725822dbfc04124eda3c390f748f8051
harshbbcspl <harshbbcspl@gmail.com>
2025-09-30 16:38:53 +0530

    30-09-2025

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Contact } from './pages/contact/contact';
+import { About } from './pages/about/about';
+import { Footer } from './pages/footer/footer';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
